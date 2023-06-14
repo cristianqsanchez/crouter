b82483e7c7460cceda7557a60fdd74c149e27851
cristianqsanchez <cristianquintero316@gmail.com>
2023-06-13 23:05:27 -0500

    feat: :sparkles: add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import Link from '../Link'
+
+export default function Page404 () {
+  return (
+    <>
+      <h1>mmm something is wrong here...</h1>
+      <img src='https://midu.dev/images/this-is-fine-404.gif' alt='Gif perro This is Fine' />
+      <Link to='/'>Volver al Home</Link>
+    </>
+  )
+}
