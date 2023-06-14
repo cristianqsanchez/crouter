4071150267068da23e5f3e29f3d6390e15686ed6
cristianqsanchez <cristianquintero316@gmail.com>
2023-06-13 23:04:38 -0500

    feat: :sparkles: add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Link from '../Link'
+
+export default function HomePage () {
+  return (
+    <>
+      <h1>home</h1>
+      <p>pagina de home</p>
+      <Link to='/about'>Ir a About</Link>
+    </>
+  )
+}
